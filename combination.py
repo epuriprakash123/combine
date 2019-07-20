cf5373dea2b6fb8730e0190c8a617045b8b6a67e
epuriprakash123 <48715250+epuriprakash123@users.noreply.github.com>
2019-07-20 22:06:05 +0530

    Create combination.py

---
+++ combination.py
@@ -0,0 +1,13 @@
+def nCr(n, r): 
+    return (fact(n) / (fact(r)  
+                * fact(n - r))) 
+def fact(n): 
+  
+    res = 1
+      
+    for i in range(2, n+1): 
+        res = res * i 
+          
+    return res                 
+n=int(input())
+print(int(nCr(n,2)))
